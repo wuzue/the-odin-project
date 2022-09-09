2048c21a9ea96120e80f40e8fd347077364973ce
Renan Araújo <renan.araujoonline@gmail.com>
2022-09-09 16:50:50 -0300

    added slider to change canvas size (NumberxNumber)

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -5,7 +5,7 @@ function doRow(rows, cols){
 	container.style.setProperty('--colunas', cols);
 	for (let i = 0; i < (rows * cols); i++){
 		let square = document.createElement('div');
-		square.addEventListener('click', () => square.style.backgroundColor = 'red');
+		//square.addEventListener('click', () => square.style.backgroundColor = 'red');
 		//square.innerText = i;
 		container.appendChild(square).className = 'square-item ' + i;
 		
@@ -13,6 +13,30 @@ function doRow(rows, cols){
 };
 
 doRow(16,16);
+
+function removeAllChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+const slider = document.querySelector('#slider')
+const screenVal = document.querySelector('.setSliderValue');
+slider.addEventListener('input', function(){
+    let val = document.getElementById('slider').value;
+	valuetwo = val
+    screenVal.textContent = val + 'x' + valuetwo;
+    removeAllChildNodes(container);
+    container.setAttribute('style', `grid-template-columns: repeat(${val}, 2fr); grid-template-rows: repeat(${val}, 2fr);`);
+    for (let i = 0; i < val*val; i++) {
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+        container.appendChild(div); 
+    }
+});
 
 // const selectSquare = document.querySelector(".square-item");
 
