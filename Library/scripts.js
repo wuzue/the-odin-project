3199060f9f2117bb4d20f0c6f82f35a1fdecfb66
Renan raújo <renan.araujoonline@gmail.com>
2022-09-21 09:08:27 -0300

    able to add new books from form

--- Library/scripts.js
+++ Library/scripts.js
@@ -1,55 +1,56 @@
 class Book {
-    constructor(title, author, category){
+    constructor(title,author,category) {
         this.title = title;
         this.author = author;
         this.category = category;
     }
 }
 
-let btn = document.querySelector('#submit');
-let elementUl = document.querySelector('#book-list')// <ul> ADD TO INDEX, RIGHT AFTER FORM TAG
+let btn = document.getElementById('submit');
+let elementUl = document.getElementById('book-list');
 
 let books = [];
 
-function addElement(book){
+function addElement(book) {
     
-    //switch to p, to hold book information
+    // Convert these elements to p, in order to hold your book info.    
     let elementTitle = document.createElement('p');
     let elementAuthor = document.createElement('p');
     let elementCategory = document.createElement('p');
-
-    //hold book information
+    
+    // The list element that will hold all book info.
     let bookElement = document.createElement('li');
 
-    let titleContent = document.createTextNode('Title: ' + book.title);
-    let authorContent = document.createTextNode('Author: ' + book.author);
-    let categoryContent = document.createTextNode('Category: ' + book.category);
+    let titleContent = document.createTextNode("Title: " + book.title);
+    let authorContent = document.createTextNode("Author: " + book.author);
+    let categoryContent = document.createTextNode("Category: " + book.category);
 
     elementTitle.appendChild(titleContent);
     elementAuthor.appendChild(authorContent);
     elementCategory.appendChild(categoryContent);
-
-    //add book information to list
+    
+    // Add all book info to list element.
     bookElement.appendChild(elementTitle);
     bookElement.appendChild(elementAuthor);
     bookElement.appendChild(elementCategory);
-
-    //add list to list element
+    
+    // Add list element to list.
     elementUl.appendChild(bookElement);
 }
 
-function submitBook(e){
-    e.preventDefault();
+function submitBook(event) {
+    event.preventDefault();
+    
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let category = document.getElementById('category').value;
 
-    let title = document.querySelector('title').value;
-    let author = document.querySelector('author').value;
-    let category = document.querySelector('category').value;
+    let book = new Book(title,author,category);
 
-    let book = new Book(title, author, category);
+    books.push(book);     
 
-    books.push();
-
-    addElement();
+    addElement(book);
+      
 }
 
-btn.addEventListener('click', submitBook);
+btn.addEventListener('click',submitBook);
