7a40840a1a7bab29aab6264263410bdb9d7abbc9
Renan raújo <renan.araujoonline@gmail.com>
2022-09-24 22:29:03 -0300

    tittactoe backup

--- tictactoe/app/main.js
+++ tictactoe/app/main.js
@@ -1,21 +1,3 @@
-/*var drawBoard = (x, y, width, height) => {
-    var box = document.querySelector('#board');
-
-    box.width = 1920;
-    box.height = 1080;
-    if (box.getContext){
-        drawBox = box.getContext('2d');
-
-        return drawBox.strokeRect(x, y, width, height);
-
-    }
-}
-
-var insideBoard = (x, y, width, height) => {
-    var 
-} 
-
-drawBoard(650, 50, 500, 500)*/
 var drawBoard = (rows, cols) => {
     box = document.querySelector("#board-div");
     box.style.setProperty('--linhas', rows);
@@ -23,8 +5,16 @@ var drawBoard = (rows, cols) => {
     for(let i = 0; i < (rows * cols); i++){
         let gameBoard = document.createElement('div');
         //gameBoard.setAttribute('id', 'game-board');
-        box.appendChild(gameBoard).className = 'square-item ' + i;
+        box.appendChild(gameBoard).className = 'square-item';
     };
 };
 
 drawBoard(3,3);
+
+var square = document.querySelector('.square-item')
+square.addEventListener('click', function(){
+    console.log(this.square);
+    console.log('oi');
+
+})
+console.log(square);
