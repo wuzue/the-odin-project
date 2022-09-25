a98c12cc3615d395dd3ef0d055d0f2c680fe311b
Renan Araújo <renan.araujoonline@gmail.com>
2022-09-25 16:57:40 -0300

    count click on each element independently

--- tictactoe/app/main.js
+++ tictactoe/app/main.js
@@ -3,7 +3,7 @@ var drawBoard = (rows, cols) => {
     box.style.setProperty('--linhas', rows);
     box.style.setProperty('--colunas', cols);
     for(let i = 0; i < (rows * cols); i++){
-        let gameBoard = document.createElement('div');
+        gameBoard = document.createElement('div');
         //gameBoard.setAttribute('id', 'game-board');
         box.appendChild(gameBoard).className = 'square-item';
     };
@@ -11,10 +11,10 @@ var drawBoard = (rows, cols) => {
 
 drawBoard(3,3);
 
-var square = document.querySelector('.square-item')
-square.addEventListener('click', function(){
-    console.log(this.square);
-    console.log('oi');
+var square = document.getElementsByClassName('square-item')
 
-})
-console.log(square);
+for(let i = 0; i < square.length; i++){
+    square[i].addEventListener('click', function(){
+        this.classList.toggle(console.log('click @ ' + i));
+    })
+}
