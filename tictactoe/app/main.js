fb2f67fca79e077426556d8f6f2867d571b60660
Renan Araújo <renan.araujoonline@gmail.com>
2022-09-26 15:30:42 -0300

    fixed plays getting overwritten

--- tictactoe/app/main.js
+++ tictactoe/app/main.js
@@ -6,15 +6,56 @@ var drawBoard = (rows, cols) => {
         gameBoard = document.createElement('div');
         //gameBoard.setAttribute('id', 'game-board');
         box.appendChild(gameBoard).className = 'square-item';
+        box.addEventListener('click',  clickGameBox);
     };
 };
 
 drawBoard(3,3);
 
-var square = document.getElementsByClassName('square-item')
+square = document.getElementsByClassName('square-item')
 
-for(let i = 0; i < square.length; i++){
-    square[i].addEventListener('click', function(){
-        this.classList.toggle(console.log('click @ ' + i));
+let playerOne = 'x';
+let playerTwo = 'o';
+let turn = 0;
+let lastClicked;
+
+/*for(let i = 0; i < square.length; i++){ THIS THING HERE IS WORKING, ITS
+    square[i].addEventListener('click', function(){ GETTING THE CLICK IN EACH SQUARE
+        this.classList.toggle(console.log('click @ ' + i + drawX)); IN THE BOX
     })
+}*/
+
+
+function clickGameBox(e){
+    if (turn === 0){
+        e.target.innerText = playerOne;
+        lastClicked = playerOne;
+    }else if(e.target.innerHTML != ""){
+        return;
+    }else{
+        if(lastClicked === playerOne){
+            e.target.innerText = playerTwo;
+            lastClicked = playerTwo;
+        } else {
+            e.target.innerText = playerOne;
+            lastClicked = playerOne;
+        }
+    }
+    turn = turn + 1;
 }
+
+/*
+function clickGameBox(){
+    var whosTurn = true;
+    function turn(id){
+        var element = square;
+        if(whosTurn){
+            element.innerHTML = 'X';
+        } else {
+            element.innerHTML = 'O';
+        }
+        element.disabled = true;
+        whosTurn = !whosTurn;
+    }turn();
+}
+*/
