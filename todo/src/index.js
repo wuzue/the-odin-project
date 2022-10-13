5284e2073601412d0ef2c1285c9a3bfe1612be5a
Renan Araújo <renan.araujoonline@gmail.com>
2022-10-13 09:32:04 -0300

    can add new tasks and filter by completed, uncompleted and all

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,53 +1,126 @@
 //IMPORTS
 import '../assets/styles/index.css';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
-//SELECTORS
-
-//FUNCTIONS | DOM
+//CREATE ELEMENTS
 let mainContainer = document.createElement('div');
-mainContainer.setAttribute('id', 'main-container');
+mainContainer.classList.add('main-div');
 document.body.appendChild(mainContainer);
+
+let filterDiv = document.createElement('div');
+filterDiv.classList.add('select');
+mainContainer.appendChild(filterDiv);
+
+let filter = document.createElement('select');
+filter.classList.add('filter-todo')
+filterDiv.appendChild(filter);
+
+let optionOne = document.createElement('option');
+optionOne.setAttribute('value', 'all');
+optionOne.textContent = 'All';
+filter.appendChild(optionOne);
+
+let optionTwo = document.createElement('option');
+optionTwo.setAttribute('value', 'completed');
+optionTwo.textContent = 'Completed';
+filter.appendChild(optionTwo);
+
+let optionThree = document.createElement('option');
+optionThree.setAttribute('value', 'uncompleted');
+optionThree.textContent = 'Uncompleted';
+filter.appendChild(optionThree);
 
 let inputTask = document.createElement('input');
 inputTask.setAttribute('type', 'text');
-inputTask.setAttribute('id', 'input-task');
+inputTask.placeholder = 'Task...';
+inputTask.classList.add('input-task');
 mainContainer.appendChild(inputTask);
 
 let inputBtn = document.createElement('button');
 inputBtn.setAttribute('type', 'submit');
-inputBtn.setAttribute('id', 'input-btn');
-inputBtn.innerText = 'Add'
+inputBtn.classList.add('input-button')
+inputBtn.innerHTML = `<i class='fas fa-plus'></i>`
 mainContainer.appendChild(inputBtn);
 
-let storeTask = document.createElement('ul');
-storeTask.id = 'store-task'
-mainContainer.appendChild(storeTask);
+let taskList = document.createElement('ul');
+taskList.classList.add('task-list');
+mainContainer.appendChild(taskList);
 
-inputBtn.addEventListener('click', writeTask());
+//SELECTORS
+let filterOption = document.querySelector('.filter-todo');
 
-function writeTask(){
-    inputBtn.addEventListener('click', () => {
-        let data = document.getElementById('input-task').value;
-        let dataToP = document.createElement('p');
-        dataToP.id = 'task-item';
-        dataToP.innerText = data;
-        document.getElementById('input-task').value = '';
-        storeTask.appendChild(dataToP);
-        
-        let delItem = document.createElement('p');
-        let delItemContent = document.createTextNode('Done');
-        let taskElement = document.createElement('li');
-        
-        taskElement.setAttribute('class', 'li-tasks');
-        taskElement.appendChild(dataToP);
-        delItem.setAttribute('id', 'delete-task');
-        
-        delItem.appendChild(delItemContent);
-        taskElement.appendChild(delItem);
-        storeTask.appendChild(taskElement);
+//EVENT LISTENERS
+inputBtn.addEventListener('click', addTask);
+taskList.addEventListener('click', deleteTodo);
+filterOption.addEventListener('click', filterTodo);
 
-        delItem.addEventListener('click', () => {
-            taskElement.remove();
+//FUNCTIONS
+function addTask(){
+    let todoDiv = document.createElement('div');
+    let taskDataToLi = document.createElement('li');
+    let checkButton = document.createElement('button');
+    let deleteButton = document.createElement('button');
+
+    let taskData = document.querySelector('.input-task').value;
+    checkButton.innerHTML = `<i class="fas fa-check"></i>`;
+    deleteButton.innerHTML = `<i class="fas fa-trash"></i>`;
+    
+
+    todoDiv.classList.add('todo');
+    checkButton.classList.add('check-button');
+    deleteButton.classList.add('delete-button');
+
+    taskDataToLi.innerText = taskData;
+
+    
+    todoDiv.appendChild(taskDataToLi);
+    todoDiv.appendChild(checkButton);
+    todoDiv.appendChild(deleteButton);
+    document.querySelector('.input-task').value = '';
+    taskList.appendChild(todoDiv);
+}
+
+function deleteTodo(e){
+    let element = e.target;
+    let i = element.parentElement;
+    console.log(i);
+    if(element.classList[0] === 'delete-button'){
+        let todo = element.parentElement;
+        todo.classList.add('fall');
+        todo.addEventListener('transitioned', e => {
+            todo.remove();
         })
+    }
+    if(element.classList[0] === 'check-button'){
+        let todo = element.parentElement;
+        todo.classList.toggle('completed');
+        console.log(todo);
+    }
+}
+
+function filterTodo(e){
+    let todos = taskList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = 'flex';
+                break;
+            case "completed":
+                if(todo.classList.contains("completed")){
+                    todo.style.display = 'flex';
+                }else{
+                    todo.style.display = 'none';
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains("completed")){
+                    todo.style.display = 'flex';
+                }else{
+                    todo.style.display = 'none';
+                }
+        }
     })
 }
